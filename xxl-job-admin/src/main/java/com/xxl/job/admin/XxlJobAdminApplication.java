19ce76cea215cd975265f7bb4871afc4fd5cf4c1
loaf199381515 <mengdonghui@wavewisdom.com>
2019-08-27 17:13:50 +0800

    去除acm配置

--- xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
@@ -1,22 +1,12 @@
 package com.xxl.job.admin;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author xuxueli 2018-10-28 00:38:13
  */
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@ComponentScan(basePackages ="com.xxl.job.admin.*")
-@EnableTransactionManagement
-@ServletComponentScan("com.xxl.job.admin.*")
-@MapperScan(basePackages = "com.xxl.job.admin.dao")
+@SpringBootApplication()
 public class XxlJobAdminApplication {
 
 	public static void main(String[] args) {
--- xxl-job-admin/src/main/java/com/xxl/job/admin/controller/JobInfoController.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/controller/JobInfoController.java
@@ -17,8 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.wave.centimani.tool.component.log.LogEnum;
-import com.wave.centimani.tool.component.log.LogsUtil;
 import com.xxl.job.admin.controller.annotation.PermissionLimit;
 import com.xxl.job.admin.core.exception.XxlJobException;
 import com.xxl.job.admin.core.model.XxlJobGroup;
@@ -157,7 +155,8 @@ public class JobInfoController {
     @ResponseBody
     @PermissionLimit(limit = false)
     public ReturnT<String> adds(@RequestBody XxlJobInfo jobInfo) {
-        // LogsUtil.setLogInfo("", LogEnum.IN, "xxl-job", "adds", null, "", jobInfo, "xxl-job-post-adds添加");
+    	logger.info("[excute method:添加任务]["+jobInfo.toString()+"]");
+        //LogsUtil.setLogInfo("", LogEnum.IN, "xxl-job", "adds", null, "", jobInfo, "xxl-job-post-adds添加");
         return xxlJobService.add(jobInfo);
     }
     /**
@@ -169,6 +168,7 @@ public class JobInfoController {
     @ResponseBody
     @PermissionLimit(limit = false)
     public ReturnT<String> updates(@RequestBody XxlJobInfo jobInfo) {
+    	 logger.info("[excute method:更新任务]["+jobInfo.toString()+"]");
         // LogsUtil.setLogInfo("", LogEnum.IN, "xxl-job", "updates", null, "", jobInfo, "xxl-job-post-updates更新");
         return xxlJobService.update(jobInfo);
     }
@@ -181,6 +181,7 @@ public class JobInfoController {
     @ResponseBody
     @PermissionLimit(limit = false)
     public ReturnT<String> removes(@RequestBody XxlJobInfo jobInfo) {
+    	 logger.info("[excute method:删除任务]["+jobInfo.toString()+"]");
         // LogsUtil.setLogInfo("", LogEnum.IN, "xxl-job", "removes", null, "", jobInfo, "xxl-job-post-removes移除");
         return xxlJobService.remove(jobInfo.getId());
     }
@@ -193,6 +194,7 @@ public class JobInfoController {
     @ResponseBody
     @PermissionLimit(limit = false)
     public ReturnT<String> pauses( @RequestBody XxlJobInfo jobInfo) {
+    	 logger.info("[excute method:停止任务]["+jobInfo.toString()+"]");
         // LogsUtil.setLogInfo("", LogEnum.IN, "xxl-job", "stops", null, "", jobInfo, "xxl-job-post-stops停止");
         return xxlJobService.stop(jobInfo.getId());
     }
@@ -205,6 +207,7 @@ public class JobInfoController {
     @ResponseBody
     @PermissionLimit(limit = false)
     public ReturnT<String> starts(@RequestBody XxlJobInfo jobInfo) {
+    	logger.info("[excute method:启动任务]["+jobInfo.toString()+"]");
         // LogsUtil.setLogInfo("", LogEnum.IN, "xxl-job", "starts", null, "", jobInfo, "xxl-job-post-starts启动");
         return xxlJobService.start(jobInfo.getId());
     }
@@ -218,6 +221,7 @@ public class JobInfoController {
     @ResponseBody
     @PermissionLimit(limit = false)
     public ReturnT<String> triggerJobs(@RequestBody XxlJobInfo jobInfo) {
+    	logger.info("[excute method:立刻执行任务]["+jobInfo.toString()+"]");
       //  LogsUtil.setLogInfo("", LogEnum.IN, "xxl-job", "triggers", null, "", jobInfo, "xxl-job-post-triggers立即触发");
 
         if (jobInfo.getExecutorParam() == null) {
